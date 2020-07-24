1482ffba86d9ad09e6ce586f834fe40ab9629971
rustam217 <rusich.krutich02@gmail.com>
2020-07-24 17:57:13 +0600

    first commit

---
+++ homeWork11/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace homeWork11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student(("Shamiev", "Rustam", "Ruslanovich"), "CS2001",
+                                            "IT-academy", "C#", 21,(20,2,1999),"Male");
+            student1.Info();
+
+            Console.WriteLine("-------------------------------------------------" +
+                              "\nCalled SetFullName method :");
+
+            student1.SetFullName("Sabyrov", "Audin", "");
+
+            Console.WriteLine($"After set Empty field: {student1.FullName}");
+
+
+           student1.SetFullName("Sabyrov", "Audin", "Nurlanovich");
+            Console.WriteLine(student1.FullName);
+
+            Console.WriteLine("-------------------------------------------------" +
+                              "\nCalled GetFullName method :");
+
+            Console.WriteLine(student1.GetFullName());
+
+            Console.WriteLine("-------------------------------------------------" +
+                             "\nCalled Info method :");
+
+            student1.Info();
+ 
+            Console.WriteLine("-------------------------------------------------" +
+                             "\nCalled Transfer method :");
+            student1.Transfer("","Smaiti-academy");
+
+            Console.WriteLine($"After set Empty field: {student1.Group + ", " + student1.Faculty}");
+           
+            student1.Transfer("JAVA2001", "Smaiti-academy");
+
+            Console.WriteLine(student1.Group + " , " + student1.Faculty);
+        }
+    }
+}
---
+++ homeWork11/Student.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homeWork11
+{
+    class Student
+    { 
+        public (string surname, string name, string patronymic) FullName { get; private set; }
+        public string Group { get; private set; }
+        public string Faculty { get; private set; }
+        public string Course { get; private set; }
+        public int Age { get; private set; }
+        public (byte day, byte month, int year) Birthday { get; private set; }
+        public string Gender { get; private set; }
+
+        public Student() { }
+
+        public Student ((string surname, string name, string patronymic)fullName, 
+
+                       string group, string faculty, string course,
+
+                       int age, (byte day,byte month, int year) birthday, string gender)
+        {
+            FullName = fullName;
+            Group = group;
+            Faculty = faculty;
+            Course = course;
+            Age = age;
+            Birthday = birthday;
+            Gender = gender;
+        }
+
+        public void SetFullName(string surname, string name, string patronymic)
+        {
+            if (name == "" || surname == "" || patronymic == "") Console.WriteLine("\nEmpty value!");
+            else FullName = (surname, name, patronymic);
+        }
+
+        public string GetFullName()
+        {
+            return FullName.surname + " " + FullName.name + " " + FullName.patronymic;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"Name: {FullName}\n"+ 
+                              $"\nAge: {Age}\n" + 
+                              $"\nDate of Birth: {Birthday}\n" + 
+                              $"\nGender: {Gender}\n" + 
+                              $"\nGroup: {Group}\n" + 
+                              $"\nFaculty: {Faculty}\n" + 
+                              $"\nCourse: {Course}\n");
+        }
+        public void Transfer(string group, string faculty)
+        {
+            if (group == "" || faculty == "") Console.WriteLine("\nEmpty value!");
+            else
+            {
+                Group = group; 
+                Faculty = faculty;
+            }
+        }
+
+
+
+    }
+}
